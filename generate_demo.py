c97a8f000ff88ed14da95de29698d464b1cc96f6
Luotonghahaha <2629366933@qq.com>
2024-05-13 21:43:09 +0800

    compare the ground truth with the pred images producing by the trained model

--- generate_demo.py
+++ generate_demo.py
@@ -1,23 +1,21 @@
-import torch
-import torch.nn as nn
-# from models.pre_model import UnetModel
-from models.models import Encoder, Decoder, RNNModel
-from evaluation.ssim import ssim
-from evaluation.psnr import psnr
-from config_para import cfg
+import matplotlib.pyplot as plt
 import numpy as np
+import torch
 from torchvision import transforms as T
+
 import os
+from config_para import cfg
+# from models.pre_model import UnetModel
+from models.lstm_model import Encoder, Decoder, ConvLSTM
 
 index = 0
 sample_path = cfg.test_path
 test_npy_path = cfg.test_npy_path
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 test_transform = T.Compose([
-                            T.ToPILImage(),
-                            T.ToTensor(),
-                            T.Normalize((0.1307,), (0.3081,))
-                            ])
+    T.ToPILImage(),
+    T.ToTensor(),
+])
 
 
 def demo_sample(index, sample_path, test_npy_path):
@@ -34,63 +32,62 @@ def demo_sample(index, sample_path, test_npy_path):
     for i in range(video_np.shape[0]):
         for j in range(video_np.shape[1]):
             video_tensor[i, j] = test_transform(video_np[i, j])
-    data = torch.tensor(video_np[:, 0:-1, :, :])
+    data = torch.tensor(video_np[:, 0:-1, :, :]).unsqueeze(2)
     target = torch.tensor(video_np[:, -1, :, :]).unsqueeze(1)
     return data, target
 
 
 demo_data, demo_target = demo_sample(index, sample_path, test_npy_path)
-model_para = './Logs/UnetModel_1710248296/ckpt/best_psnr.pth'
 
-# model = UnetModel().to(device)
-encoder = Encoder().to(device)
-latent_model = RNNModel(cfg.hidden_rnn, cfg.hidden_rnn).to(device)
-decoder = Decoder().to(device)
-h_0 = torch.zeros(1, 6, cfg.hidden_rnn).to(device)
+# 实例化模型
+convlstm_forward = ConvLSTM(input_shape=(16, 16), input_dim=64, hidden_dims=[128, 128, 64], n_layers=3,
+                            kernel_size=(3, 3),
+                            device=device)
+encoder = Encoder(device)
+decoder = Decoder(device)
 
-# encoder.load_state_dict(torch.load(model_para))
-# latent_model.load_state_dict(torch.load(model_para))
-# decoder.load_state_dict(torch.load(model_para))
+# 加载已经保存的模型
+checkpoint_encoder = torch.load('./Logs/ckpt/last_encoder.pth', map_location=device)
+checkpoint_lstm_forward = torch.load('./Logs/ckpt/last_lstm_forward.pth', map_location=device)
+checkpoint_decoder = torch.load('./Logs/ckpt/last_decoder.pth', map_location=device)
 
-# pred = model(demo_data.float())
+encoder.load_state_dict(checkpoint_encoder['state_dict'])
+convlstm_forward.load_state_dict(checkpoint_lstm_forward['state_dict'])
+decoder.load_state_dict(checkpoint_decoder['state_dict'])
 
+pred_list = []
+lstm_for_input = demo_data[:, cfg.interval - 1]
+for ti in range(cfg.target_num):
+    # print('forward prediction')
+    encoder_forward_pred = encoder(lstm_for_input.to(torch.float32))
+    hidden_forward_pred, output_forward_pred = convlstm_forward(encoder_forward_pred, ti == 0)
+    decoder_forward_pred = decoder(output_forward_pred[-1])
+    pred_list.append(decoder_forward_pred)
 
-# # 单向RNN
-# # print(demo_data.shape)
-# hidden_x = encoder(demo_data.float().unsqueeze(2).transpose(0, 1))
-# # print(hidden_x.shape)
-# hidden_y, _ = latent_model(hidden_x.view(cfg.interval * 2, cfg.batch_size, cfg.hidden_rnn * 1 * 1), h_0)
-# pred_unirnn = decoder(hidden_y)
+    # print(loss_forward_pred)
+    lstm_for_input = decoder_forward_pred
 
-# 双向RNN
-hidden_x = encoder(demo_data.float().unsqueeze(2).transpose(0, 1))
-forward_y, _ = latent_model(hidden_x[0: 2].view(cfg.interval, 6, cfg.hidden_rnn * 1 * 1), h_0)
-reverse_y, _ = latent_model(torch.flip(hidden_x[2:], dims=[0]).view(cfg.interval, 6, cfg.hidden_rnn * 1 * 1), h_0)
-hidden_y = cfg.epsilon * forward_y + (1 - cfg.epsilon) * reverse_y
+inter_pred = torch.stack(pred_list, dim=1)
 
-pred_birnn = decoder(hidden_y.view(6, cfg.hidden_rnn, 1, 1))
+fig, axes = plt.subplots(nrows=2, ncols=10, figsize=(20, 6))
+# 显示 ground truth 图像
+for i, ax in enumerate(axes[0]):
+    ax.imshow(demo_target[i, 0].squeeze(0), cmap='gray')
+    ax.set_title('T='+str(i+cfg.interval))
+    ax.axis('off')
 
-# # 单向RNN
-# uni_rnn_demo_path = './out/generate/uni_rnn'
-# toPIL = T.ToPILImage()
-# if not os.path.exists(uni_rnn_demo_path):
-#     os.makedirs(uni_rnn_demo_path)
-# for i in range(demo_data.shape[0]):
-#     for j in range(cfg.interval):
-#         toPIL(demo_data[i, j]).save(uni_rnn_demo_path + '/' + str((cfg.interval + 1) * i + j) + '.png')
-#     toPIL(pred_unirnn[i]).save(uni_rnn_demo_path + '/' + str((cfg.interval + 1) * i + 1) + '_' + str((cfg.interval + 1) * i + 3) + '.png')
-# for i in range(cfg.interval):
-#      toPIL(demo_data[-1, cfg.interval+i]).save(uni_rnn_demo_path + '/' + str(cfg.T - (i+1)) + '.png')
+# 显示预测图像
+for i, ax in enumerate(axes[1]):
+    ax.imshow(inter_pred[i, 0].squeeze(0).detach().numpy(), cmap='gray')
+    ax.set_title('T='+str(i+cfg.interval))
+    ax.axis('off')
 
-# 双向RNN
-bi_rnn_demo_path = './out/generate/bi_rnn'
-toPIL = T.ToPILImage()
-if not os.path.exists(bi_rnn_demo_path):
-    os.makedirs(bi_rnn_demo_path)
+plt.suptitle("Ground Truth vs. Prediction")
+plt.tight_layout()
 
-for i in range(demo_data.shape[0]):
-    for j in range(cfg.interval):
-        toPIL(demo_data[i, j]).save(bi_rnn_demo_path + '/' + str((cfg.interval + 1) * i + j) + '.png')
-    toPIL(pred_birnn[i]).save(bi_rnn_demo_path + '/' + str((cfg.interval + 1) * i + 1) + '_' + str((cfg.interval + 1) * i + 3) + '.png')
-for i in range(cfg.interval):
-     toPIL(demo_data[-1, cfg.interval+i]).save(bi_rnn_demo_path + '/' + str(cfg.T - (i+1)) + '.png')
+save_folder = './out'
+os.makedirs(save_folder, exist_ok=True)
+
+save_path = os.path.join(save_folder, 'ground_truth_vs_prediction.png')
+plt.savefig(save_path)
+
